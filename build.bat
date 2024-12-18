584517cfb6c98adf21c57ec0cebaeae6dd76ad63
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2024-12-18 19:07:18 +0200

    changing fonts update!

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 windres src.rc -O coff -o src.res
-gcc src.res texteitorer.c -o texteitorer.exe -mwindows
+gcc src.res texteitorer.c -o texteitorer.exe -lgdi32 -lcomdlg32 -mwindows
 del src.res
--- texteitorer.c
+++ texteitorer.c
@@ -5,6 +5,7 @@
 #define ABUT 256
 #define SAVE 512
 #define FSIZ 1024
+#define OPEN 2048
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,7 +20,44 @@ int rX = 0;
 int rY = 0;
 int rXV = 5;
 int rYV = 5;
+HWND hwnd;
 HWND edit;
+COLORREF winCol;
+
+COLORREF initWinCol() {
+	COLORREF winCol = GetBkColor(GetDC(hwnd));
+	return winCol;
+}
+
+HBRUSH mkBgHbrush(COLORREF color) {
+	BITMAPINFO bmi;
+	ZeroMemory(&bmi, sizeof(BITMAPINFO));
+	bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bmi.bmiHeader.biWidth = 1;
+	bmi.bmiHeader.biHeight = 1;
+	bmi.bmiHeader.biPlanes = 1;
+	bmi.bmiHeader.biBitCount = 32;
+	bmi.bmiHeader.biCompression = BI_RGB;
+	HDC hdc = GetDC(NULL);
+	void* pBits;
+	HBITMAP hBmp = CreateDIBSection(hdc, &bmi, DIB_RGB_COLORS, &pBits, NULL, 0);
+	ReleaseDC(NULL, hdc);
+	HBRUSH hBg = NULL;
+	if (hBmp) {
+		COLORREF* pixels = (COLORREF*)pBits;
+		*pixels = color;
+		hBg = CreatePatternBrush(hBmp);
+	}
+	return hBg;
+}
+
+COLORREF hbrushToColorref(HBRUSH hBrush) {
+	COLORREF col = CLR_INVALID;
+	LOGBRUSH lb;
+	int result = GetObject(hBrush, sizeof(LOGBRUSH), &lb);
+	if ((result) && (lb.lbStyle == BS_SOLID)) col = lb.lbColor;
+	return col;
+}
 
 bool triggerSaveAs() {
 	OPENFILENAME ofn;
@@ -60,16 +98,97 @@ HMENU populateMenu() {
 	AppendMenu(fileM, MF_STRING, ABUT, L"aboot");
 	AppendMenu(fileM, MF_SEPARATOR, 0, NULL);
 	AppendMenu(fileM, MF_STRING, SAVE, L"sane");
+	AppendMenu(fileM, MF_STRING, OPEN, L"oven");
 	// file
 	// edit
-	AppendMenu(editM, MF_STRING, FSIZ, L"fort sine....");
+	AppendMenu(editM, MF_STRING, FSIZ, L"fort....");
 	// edit
 	AppendMenu(res, MF_POPUP, (UINT_PTR)fileM, L"filet");
 	AppendMenu(res, MF_POPUP, (UINT_PTR)editM, L"emit");
 	return res;
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+		case WM_COMMAND:
+			switch (LOWORD(wParam)) {
+				case EXIT:
+					SendMessage(hwnd, WM_CLOSE, 0, 0);
+					break;
+				case ABUT:
+					MessageBox(hwnd, L"text eitorer\n\nmade by grad man 2 be silly", L"aboot", MB_OK | MB_ICONINFORMATION);
+					break;
+				case SAVE:
+					triggerSaveAs();
+					break;
+				case FSIZ:
+					CHOOSEFONT cf;
+					HFONT hf;
+					LOGFONT lf;
+					ZeroMemory(&cf, sizeof(cf));
+					ZeroMemory(&lf, sizeof(lf));
+					GetObject((HFONT)SendMessage(edit, WM_GETFONT, 0, 0), sizeof(LOGFONT), &lf);
+					cf.lStructSize = sizeof(cf);
+					cf.hwndOwner = hwnd;
+					// cf.hInstance = 
+					cf.lpLogFont = &lf;
+					cf.Flags = CF_SCREENFONTS | CF_SHOWHELP | CF_SCALABLEONLY | CF_FORCEFONTEXIST | CF_INITTOLOGFONTSTRUCT;
+					BOOL font = ChooseFont(&cf);
+					SendMessage(edit, WM_SETFONT, (WPARAM)(CreateFontIndirect(&lf)), true);
+					break;
+				case OPEN: // stub
+					break;
+			}
+			return 0;
+		case WM_CLOSE:
+			int result = MessageBoxW(hwnd, L"doh yo went two sane beform exciting text eitorer?", L"text eitorer", MB_YESNOCANCEL | MB_ICONWARNING);
+			if ((result == IDCANCEL) || ((result == IDYES) && (!(triggerSaveAs())))) return 0;
+			DestroyWindow(hwnd);
+			return 0;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+		case WM_CTLCOLOREDIT:
+			break;
+			HDC hdcEdit = (HDC)wParam;
+			initWinCol();
+			SetBkColor(hdcEdit, TRANSPARENT);
+			return (INT_PTR)(initWinCol());
+		/*
+			case WM_PAINT:
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(hwnd, &ps);
+				FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+				// rectangle ig
+				HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
+				HPEN hPenInvisible = CreatePen(PS_SOLID, 0, GetSysColor(COLOR_WINDOW));
+				HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrushRed);
+				HPEN hOldPen = (HPEN)SelectObject(hdc, hPenInvisible);
+				Rectangle(hdc, rX, rY, 100 + rX, 100 + rY);
+				SelectObject(hdc, hOldBrush);
+				SelectObject(hdc, hOldPen);
+				DeleteObject(hBrushRed);
+				DeleteObject(hPenInvisible);
+				// rectangle ig
+				EndPaint(hwnd, &ps);
+				return 0;
+			case WM_TIMER:
+				rX += rXV;
+				rY += rYV;
+				InvalidateRect(hwnd, NULL, 1);
+				return 0;
+			case WM_KEYDOWN:
+				if ((wParam == 'A') && (GetKeyState(VK_CONTROL) & 0x8000)) { // Check if Ctrl is pressed
+					SendMessage(edit, EM_SETSEL, 0, -1); // Select all text in the edit control
+					return 0;
+				}
+				break;
+		*/
+		default:
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
 	const wchar_t CLASS_NAME[] = L"TextEitorer";
@@ -96,6 +215,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 		hInstance, // an instance of h (100% NOT CLICKBAIT!!!!)
 		NULL // useless app data
 	);
+	// winCol = ;
 	if (hwnd == NULL) {
 		return 0;
 	}
@@ -134,87 +254,4 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
 		DispatchMessage(&msg);
 	}
 	return 0;
-}
-
-COLORREF hbrushToColorref(HBRUSH hBrush) {
-	COLORREF col = CLR_INVALID;
-	LOGBRUSH lb;
-	int result = GetObject(hBrush, sizeof(LOGBRUSH), &lb);
-	if ((result) && (lb.lbStyle == BS_SOLID)) col = lb.lbColor;
-	return col;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	switch (uMsg) {
-		case WM_COMMAND:
-			switch (LOWORD(wParam)) {
-				case EXIT:
-					SendMessage(hwnd, WM_CLOSE, 0, 0);
-					break;
-				case ABUT:
-					MessageBox(hwnd, L"text eitorer\n\nmade by grad man 2 be silly", L"aboot", MB_OK | MB_ICONINFORMATION);
-					break;
-				case SAVE:
-					triggerSaveAs();
-					break;
-				case FSIZ:
-					CHOOSEFONT cf;
-					LOGFONT lf;
-					ZeroMemory(&cf, sizeof(cf));
-					ZeroMemory(&lf, sizeof(lf));
-					cf.lStructSize = sizeof(cf);
-					cf.hwndOwner = hwnd;
-					// cf.hInstance = 
-					cf.lpLogFont = &lf;
-					cf.Flags = CF_SCREENFONTS | CF_SHOWHELP | CF_SCALABLEONLY | CF_FORCEFONTEXIST;
-					BOOL font = ChooseFont(&cf);
-					break;
-			}
-			return 0;
-		case WM_CLOSE:
-			int result = MessageBoxW(hwnd, L"doh yo went two sane beform exciting text eitorer?", L"text eitorer", MB_YESNOCANCEL | MB_ICONWARNING);
-			if ((result == IDCANCEL) || ((result == IDYES) && (!(triggerSaveAs())))) return 0;
-			DestroyWindow(hwnd);
-			return 0;
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			return 0;
-		case WM_CTLCOLOREDIT:
-			HDC hdcEdit = (HDC)wParam;
-			SetBkColor(hdcEdit, GetSysColor(COLOR_WINDOW));
-			return (LRESULT)GetStockObject(BLACK_BRUSH);
-		/*
-			case WM_PAINT:
-				PAINTSTRUCT ps;
-				HDC hdc = BeginPaint(hwnd, &ps);
-				FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-				// rectangle ig
-				HBRUSH hBrushRed = CreateSolidBrush(RGB(255, 0, 0));
-				HPEN hPenInvisible = CreatePen(PS_SOLID, 0, GetSysColor(COLOR_WINDOW));
-				HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrushRed);
-				HPEN hOldPen = (HPEN)SelectObject(hdc, hPenInvisible);
-				Rectangle(hdc, rX, rY, 100 + rX, 100 + rY);
-				SelectObject(hdc, hOldBrush);
-				SelectObject(hdc, hOldPen);
-				DeleteObject(hBrushRed);
-				DeleteObject(hPenInvisible);
-				// rectangle ig
-				EndPaint(hwnd, &ps);
-				return 0;
-			case WM_TIMER:
-				rX += rXV;
-				rY += rYV;
-				InvalidateRect(hwnd, NULL, 1);
-				return 0;
-			case WM_KEYDOWN:
-				if ((wParam == 'A') && (GetKeyState(VK_CONTROL) & 0x8000)) { // Check if Ctrl is pressed
-					SendMessage(edit, EM_SETSEL, 0, -1); // Select all text in the edit control
-					return 0;
-				}
-				break;
-		*/
-		default:
-			return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
